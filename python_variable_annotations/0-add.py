b46b0e048be9e0ab7392dc3e525964e256b8f7a7
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-08-28 22:03:35 +0000

    pycode

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """0-add.py - Defines a function 'add' for adding two floats."""
 
+
 def add(a: float, b: float) -> float:
     """Adds two float numbers."""
     return a + b
