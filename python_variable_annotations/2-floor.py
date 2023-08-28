1ca7d78255086b2d8ea948b29bd1191bd24261db
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-08-28 22:45:09 +0000

    2flor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""2-floor.py
+Defines a function 'floor' to calculate the floor of a float."""
+
+
+def floor(n: float) -> int:
+    """Calculates the floor of a float."""
+    return int(n)
