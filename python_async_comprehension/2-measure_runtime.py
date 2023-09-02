1e1e267cc2db6931b7a2df7aa295d9cc10711351
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-09-02 21:10:54 +0000

    2*

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,20 +3,23 @@
 2-measure_runtime.py
 Defines a coroutine 'measure_runtime' to measure the total execution time.
 """
+
 import asyncio
 from time import time
 
-async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """Measures the total execution time for async_comprehension"""
     start_time = time()
     await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
+        *[
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension()
+        ]
     )
     end_time = time()
     total_time = end_time - start_time
