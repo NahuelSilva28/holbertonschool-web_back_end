c71a57996f152738c412bb096a1930f2c95b5340
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-09-07 22:04:22 +0000

    pycodesyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Description: This module defines a function to calculate index ranges for pagination.
+Description: This module defines
+a function to calculate index ranges for pagination.
 """
 
 from typing import Tuple
