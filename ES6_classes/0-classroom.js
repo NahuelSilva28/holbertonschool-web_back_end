b7fa4729546df5c3d675d66569cb16ea9fb9ecc4
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-09-17 18:29:01 +0000

    0

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,6 +1,9 @@
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-      this._maxStudentsSize = maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
   }
-  
+}
+
+// Example usage:
+// const room = new ClassRoom(10);
+// console.log(room._maxStudentsSize);
