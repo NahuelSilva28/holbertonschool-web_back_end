fe5b9cb53e41a22ad8731c0a4d30b97126446819
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-09-17 20:17:42 +0000

    classes

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,23 @@
+export default class Car {
+    constructor(brand, motor, color) {
+      this.brand = brand;
+      this.motor = motor;
+      this.color = color;
+    }
+  
+    cloneCar() {
+      return new Car(this.brand, this.motor, this.color);
+    }
+  }
+  
+  const tc1 = new Car("Nissan", "Turbo", "Pink");
+  const tc2 = tc1.cloneCar();
+  
+  console.log(tc1);
+  console.log(tc1 instanceof Car);
+  
+  console.log(tc2);
+  console.log(tc2 instanceof Car);
+  
+  console.log(tc1 === tc2);
+  
