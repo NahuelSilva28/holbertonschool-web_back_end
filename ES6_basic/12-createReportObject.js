ece7c6d555864ee3baf6fbf6ba4af7e54d1cf1ec
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-09-17 20:02:25 +0000

    12

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,8 +1,10 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: employeesList,
-    getNumberOfDepartments(employees) {
-      return Object.keys(employees).length;
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments() {
+      return Object.keys(employeesList).length;
     },
   };
 }
