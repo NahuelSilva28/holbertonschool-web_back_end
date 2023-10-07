421cb428622ab98b9ac4730095d879a23f8474c5
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-10-07 16:51:24 +0000

    0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve('Sample API response data');
     }, 2000);
