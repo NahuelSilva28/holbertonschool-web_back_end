6e926375e14df305285636eb380b3820fecda00c
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-10-07 17:45:12 +0000

    push

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,23 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
+
+    const [userData, photoData] = await Promise.all([userPromise, photoPromise]);
+
+    return [
+      { status: 'fulfilled', value: userData },
+      { status: 'fulfilled', value: photoData },
+    ];
+  } catch (error) {
+    return [
+      { status: 'rejected', value: error },
+      { status: 'rejected', value: error },
+    ];
+  }
+}
+
+export default handleProfileSignup;
